72c6f019790bf443bc3831b105b06fd135318ea9
Katherine <krc53@case.edu>
2016-01-31 01:34:53 +0000

    LED blinkey blink

---
+++ test.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO 
+import time
+
+GPIO.setmode(GPIO.BOARD) #use pin numbering from board
+GPIO.cleanup() #ensure channels not being used
+GPIO.setup(7, GPIO.OUT) #Set pin 7 to OUT
+GPIO.setwarnings(False)
+
+while True:
+  GPIO.output(7,True)
+  print "on"
+  time.sleep(.5)
+  GPIO.output(7,False)
+  time.sleep(.5)
+  print "off"
